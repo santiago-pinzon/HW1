ef04e47d0b09776ab0e3d896514eed062eea6f56
Santiago Pinzon <38019518+santiago-pinzon@users.noreply.github.com>
2021-05-25 18:16:28 -0400

    Working ROC Curve

--- src/hw1.py
+++ src/hw1.py
@@ -19,43 +19,81 @@ classes = 2
 
 # rng
 rng = default_rng()
-pfd1 = multivariate_normal(mean=m, cov=c) #1 label
-pdf01 = multivariate_normal(mean=m01, cov=c01) #0 label < 50
-pdf00 = multivariate_normal(mean=m02, cov=c02) #0 label > 50
+pdf1 = multivariate_normal(mean=m, cov=c)  # 1 label
+pdf01 = multivariate_normal(mean=m01, cov=c01)  # 0 label < 50
+pdf00 = multivariate_normal(mean=m02, cov=c02)  # 0 label > 50
 
 # define datasets for labels
 l0Dataset = []
 l1Dataset = []
 
-#generate dataset
+# generate dataset
 for i in range(DATASIZE):
-    test = rng.uniform()
-    if test < l0:
-        subtest = rng.uniform()
-        if subtest < l0sub:
-            l0Dataset.append(rng.multivariate_normal(m01, c01))
-        else:
-            l0Dataset.append(rng.multivariate_normal(m02, c02))
-    else:
-        l1Dataset.append(rng.multivariate_normal(m, c))
+	test = rng.uniform()
+	if test < l0:
+		subtest = rng.uniform()
+		if subtest < l0sub:
+			l0Dataset.append(rng.multivariate_normal(m01, c01))
+		else:
+			l0Dataset.append(rng.multivariate_normal(m02, c02))
+	else:
+		l1Dataset.append(rng.multivariate_normal(m, c))
 
 fulldataset = l0Dataset + l1Dataset
 
-#ERM classification
-test = pdf01.pdf([3,3])
-print(test)
+# ERM classification
+scoresL0 = []
+scoresL1 = []
+for tup in l0Dataset:
+	pTupGiven1 = pdf1.pdf(tup) * 0.35
+	pTupGiven0 = pdf01.pdf(tup) * 0.325 + pdf00.pdf(tup) * 0.35
+	scoresL0.append([pTupGiven0, pTupGiven1])
 
 
-print(l0Dataset)
-plt.plot(*zip(*l0Dataset), 'bo')
-plt.plot(*zip(*l1Dataset), 'ro')
+for tup in l1Dataset:
+	pTupGiven1 = pdf1.pdf(tup) * 0.35
+	pTupGiven0 = pdf01.pdf(tup) * 0.325 + pdf00.pdf(tup) * 0.35
+	scoresL1.append([pTupGiven0, pTupGiven1])
+
+ROCVals = []
+
+
+for lambda1 in range(2000):
+	tau = lambda1/100
+	print("loop " + str(lambda1) + "/1000")
+	# expected risk classification
+	tp = 0
+	fp = 0
+	tn = 0
+	fn = 0
+	for p in scoresL0:
+		if p[1] / p[0] > tau:
+			fp = fp + 1	#classified1.append(tup)
+		else:
+			tn = tn + 1	#classified0.append(tup)
+
+	for p in scoresL1:
+		if p[1] / p[0] > tau:
+			tp = tp + 1	#classified1.append(tup)
+		else:
+			fn = fn + 1	#classified0.append(tup)
+
+	ROCVals.append([fp/(fp + tn), tp/(tp + fn)])
+
+print(ROCVals)
+
+plt.plot(*zip(*ROCVals))
+
+# plot our things
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+ax1.plot(*zip(*l0Dataset), 'bo')
+ax1.plot(*zip(*l1Dataset), 'ro')
+
+# ax2.plot(*zip(*classified0), 'bo')
+# ax2.plot(*zip(*classified1), 'ro')
+
 plt.show()
-
-plt.plot(*zip(*fulldataset), 'go')
-plt.show()
-# class-conditional Gaussian pdfs
 
 # ROC curve
 
 # theoretical optimal thresh
-print("Test")
